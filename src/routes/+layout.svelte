6a8a3a4e9666c7a8c6b2b22257970d14135a0539
Joe Boyle <jboyle617@gmail.com>
2023-09-08 17:42:13 -0400

    prettier no tabs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,63 +1,63 @@
 <script>
-	import { page } from '$app/stores';
-	import { base } from '$app/paths';
-	import { onNavigate } from '$app/navigation';
+  import { page } from '$app/stores';
+  import { base } from '$app/paths';
+  import { onNavigate } from '$app/navigation';
 
-	// https://svelte.dev/blog/view-transitions
-	onNavigate((navigation) => {
-		if (!document.startViewTransition) return;
+  // https://svelte.dev/blog/view-transitions
+  onNavigate((navigation) => {
+    if (!document.startViewTransition) return;
 
-		return new Promise((resolve) => {
-			document.startViewTransition(async () => {
-				resolve();
-				await navigation.complete;
-			});
-		});
-	});
+    return new Promise((resolve) => {
+      document.startViewTransition(async () => {
+        resolve();
+        await navigation.complete;
+      });
+    });
+  });
 
-	$: isActive = (path) => $page.url.pathname === `${base}${path}`;
+  $: isActive = (path) => $page.url.pathname === `${base}${path}`;
 </script>
 
 <nav>
-	<ul>
-		<li><a href="{base}/" class:active={isActive(base ? '' : '/')}>Home</a></li>
-		<li><a href="{base}/longer" class:active={isActive('/longer')}>Longer title</a></li>
-		<li><a href="{base}/one" class:active={isActive('/one')}>One</a></li>
-		<li><a href="{base}/two" class:active={isActive('/two')}>Two</a></li>
-	</ul>
+  <ul>
+    <li><a href="{base}/" class:active={isActive(base ? '' : '/')}>Home</a></li>
+    <li><a href="{base}/longer" class:active={isActive('/longer')}>Longer title</a></li>
+    <li><a href="{base}/one" class:active={isActive('/one')}>One</a></li>
+    <li><a href="{base}/two" class:active={isActive('/two')}>Two</a></li>
+  </ul>
 </nav>
 
 <slot />
 
 <style>
-	ul {
-		display: flex;
-		justify-content: center;
-		gap: 1em;
-		margin: 1em 0;
-	}
+  ul {
+    display: flex;
+    justify-content: center;
+    gap: 1em;
+    margin: 1em 0;
+  }
 
-	li {
-		display: inline-block;
-	}
+  li {
+    display: inline-block;
+  }
 
-	.active:after {
-		content: '';
-		display: inline-block;
-		border-bottom: 6px solid red;
-		/* position: absolute; */
-		width: 100%;
-	}
+  .active:after {
+    content: '';
+    display: inline-block;
+    border-bottom: 6px solid red;
+    /* position: absolute; */
+    width: 100%;
+  }
 
-	.active:after {
-		view-transition-name: active-page;
-	}
+  .active:after {
+    view-transition-name: active-page;
+  }
 
-	/* :root::view-transition-old(root) { */
-	/* 	/1* transform: translateX(30px); *1/ */
-	/* } */
+  /* :root::view-transition-old(root) { */
+  /* 	/1* transform: translateX(30px); *1/ */
+  /* } */
 
-	/* :root::view-transition-new(root) { */
-	/* 	/1* transform: translateX(-30px); *1/ */
-	/* } */
+  /* :root::view-transition-new(root) { */
+  /* 	/1* transform: translateX(-30px); *1/ */
+  /* } */
 </style>
